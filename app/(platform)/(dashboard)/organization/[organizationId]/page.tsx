75716bbe11b1af25a6e5d9601285fcf9901febb2
WinnardArthur <winnardarthur7@gmail.com>
2024-06-25 05:51:04 +0000

    create navbar of dashboard

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const OrganizationIdPage = () => {
+  return <div>Organization Page</div>;
+};
+
+export default OrganizationIdPage;
