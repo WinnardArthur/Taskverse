a0cd1df178f97b33526150419ea3a815acd09871
WinnardArthur <winnardarthur7@gmail.com>
2024-06-30 06:17:08 +0000

    add sonner toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
-import SWRegistration from "@/config/sw-registration";
+import { Toaster } from 'sonner';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SWRegistration />
+        <Toaster />
         {children}
       </body>
     </html>
