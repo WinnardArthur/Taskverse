a90a532832879c20dd14142f8fa16ab8af133593
WinnardArthur <winnardarthur7@gmail.com>
2024-06-30 06:18:36 +0000

    create board-creation and info

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 
+import { Separator } from "@/components/ui/separator";
 
-import { db } from "@/lib/db";
-import { Board } from "./board";
-import { Form } from "./form";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
-
   return (
-    <div>
-      <Form/>
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board title={board.title} id={board.id} key={board.id} />
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4"/>
+      <div className="px-2 md:px-4">
+        <BoardList />
       </div>
     </div>
   );
