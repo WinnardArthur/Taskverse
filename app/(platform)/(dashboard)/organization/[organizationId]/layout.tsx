98c560b1ef3a40cf3120410a173ffde45dacb363
WinnardArthur <winnardarthur7@gmail.com>
2024-07-02 10:42:23 +0000

    modify title metadata

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { startCase } from "lodash";
+
 import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "Organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
